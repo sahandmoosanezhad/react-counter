8b89c57e62a688d13fd79ce2086c721070260104
sahandmoosanezhad <81710657+sahandmoosanezhad@users.noreply.github.com>
2022-08-24 13:09:03 +0430

    React-counterApp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
+import React , { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  
+  const [counter , setCounter] = useState(0);
+
+  const counterPlus = () => {
+    setCounter(counter + 1)
+  };
+
+  const counterMines = () => {
+    setCounter(counter - 1)
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>{counter}</form>
+        <div className='Btn'>
+          <button onClick={counterPlus}>Increament</button>
+          <button onClick={counterMines}>Decreament</button>
+        </div>
       </header>
     </div>
   );
